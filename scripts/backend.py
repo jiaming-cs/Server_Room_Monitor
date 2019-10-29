f3c94c725cf3872863cffa77332837d18b37446e
Jiamng <jiamingli.cs@gmail.com>
2019-10-29 12:29:19 -0400

    new ui test

--- scripts/backend.py
+++ scripts/backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import Adafruit_DHT
 import json
 
@@ -20,5 +20,16 @@ def draw_stone():
 @web.route('/')
 def staff_page():
     return web.send_static_file('index.html')
+
+@web.rout("/subscribe/", methods = ["GET", "POST"])
+def subscribe():
+    getData = request.args # 利用request对象获取GET请求数据
+    print('获取的GET数据为：', getData) # 打印获取到的GET数据 ImmutableMultiDict([])
+    postData = request.form # 利用request对象获取POST请求数据
+    print('获取的POST数据为：', postData) # 打印获取到的POST请求 ImmutableMultiDict([('username', '456'), ('password', '789')])
+    #username = request.form.get('username') 
+    #assword = request.form.get('password')
+    #print(username,password) #456 789
+    #return '这是测试页面'
 
 
